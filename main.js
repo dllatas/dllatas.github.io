a0fe5889c5ec9da32fa782bceb20f869e5b12597
Daniel Llatas Spiers <danllatas@gmail.com>
2015-11-30 20:49:49 +0100

    Add node_modules, bootstrap

--- main.js
+++ main.js
@@ -1,45 +1,28 @@
+$ = global.jQuery = require('jquery');
+require('bootstrap');
 var Handlebars = require("handlebars");
-var $ = require("jquery");
 
 $(function () {
     "use strict";
-    var source = $("#title-template").html();
-    var template = Handlebars.compile(source);
-    var context = {
-        title: "welcome to dllatas.github.io!",
-        subtitle: "the broad spectrum of life seduces me / el amplio espectro de la vida me seduce",
-        address1: "Lima, Peru. 1989 - 2014",
-        address2: "Uppsala, Sweden. 2014 - today",
+    var templates = [["#content-placeholder",".hb-content-placeholder",{
         skills: [
-            {topic: 'Machine Learning'},
-            {topic: 'Javascript'},
-            {topic: 'Music'}
+            {topic: 'bio', detail: "Lima, Peru. 1989 - 2014. Uppsala, Sweden. 2014 - today."},
+            {topic: 'machine learning', detail: "ga, ann, deep learning"},
+            {topic: 'uix', detail: "js"},
+            {topic: 'music', detail: "guitar, blecket"}
         ]
-    };
-    var html = template(context);
-    $('.content-placeholder').html(html);
+    }],["#jumbotron-placeholder",".hb-jumbotron-placeholder",{title: "welcome to dllatas.github.io!",
+        subtitle: "the broad spectrum of life seduces me"}],["#footer-placeholder",".hb-footer-placeholder",{footer: "2015 metAleph Inc."}]];
+
+    function render(template) {
+    	for(var i=0;i<template.length;i++){
+    		var templateCompiled = Handlebars.compile($(template[i][0]).html());
+    		console.log(template[i][2]);
+    	    var html = templateCompiled(template[i][2]);
+    		$(template[i][1]).html(html);
+    	}
+    }
+
+    render(templates);
+
 });
-
-/*
-$(function () {
-  'use strict';
-  // Grab the template script
-  var theTemplateScript = $("#address-template").html();
-
-  // Compile the template
-  var theTemplate = Handlebars.compile(theTemplateScript);
-
-  // Define our data object
-  var context={
-    "city": "London",
-    "street": "Baker Street",
-    "number": "221B"
-  };
-
-  // Pass our data to the template
-  var theCompiledHtml = theTemplate(context);
-
-  // Add the compiled html to the page
-  $('.content-placeholder').html(theCompiledHtml);
-});
-*/
